81e90a1f6da903f08b4a1c1c4a75fe7b6001a9bb
Rob Balfre <robert.balfre@travelctm.com.au>
2022-08-16 15:23:52 +1000

    init

---
+++ src/lib/foo.js
@@ -0,0 +1,3 @@
+export const foo = {
+    moo: location.protocol // <-- causes error
+}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { foo } from "$lib/foo";
+
+  // let foo = location.protocol; <-- works fine
+  console.log("foo :>> ", foo);
+</script>
+
+<h1>Welcome to SvelteKit</h1>
